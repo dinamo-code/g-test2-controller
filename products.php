70e9e984fdfcd4d974243cc57211e2c4696cb7a3
7anan <7anan@email.com>
2024-08-29 11:08:49 +0200

    products

--- products.php
+++ products.php
@@ -5,11 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/global.css">
-    <link rel="stylesheet" href="CSS/productscontrol.css">
     <title>Products List</title><!--products list-->
 
     <style>
-        .headerlogo {
+                .headerlogo {
         font-size: 30px;
         position: absolute;
         font-weight: 800;
@@ -31,8 +30,10 @@
     <div >
 
         <header >
-            <div class="createlinkdiv">
-            <a href="create.php" class="createlink">Create</a>
+            <h1> Products List </h1><!--products list-->
+            <div>
+                <a href="create.php" >Create</a>
+ 
             </div>
         </header>
 
@@ -66,54 +67,56 @@
 
     </div>  
 
-    <table class="productsmaintable">
+    <table >
 
-        <!-- <thead>
+        <thead>
 
             <tr>
 
                 <th>id</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Price</th>
+                <th>Image</th><!--image-->
+                <th>Name</th><!--name-->
+                <th>Price</th><!--price-->
                 <th>Action</th>
 
             </tr>
 
-        </thead> -->
+        </thead>
 
-        <tbody class="productsmaincontainer">
+        <tbody>
 
             <?php
             include('cfg/dbconnect.php');// cfg/dbconnect.php
             $sql = "SELECT * FROM  products";//recorrect table name
             $result = mysqli_query($conn,$sql);
 
-            while($data = mysqli_fetch_array($result))
+            while($data = mysqli_fetch_array($result)){
              ?>
-             <tr class="productcontainer">
-                 <td class="productcontainerimg"><img src="<?php echo $data ['Image'];?>"></td><!--image-->
-                 <td class="productcontainerid"><span>Id : </span><?php echo $data ['id'];?></td>
-                <td class="productcontainername"><span>Name : </span><?php echo $data ['Name'];?></td><!--name-->
-                <td class="productcontainerprice"><span>Price : </span><?php echo $data ['Price'];?><span>$</span></td><!--price-->
-                <td class="productcontainerviewanddelete">
-                    <a href="view.php?id=<?php echo $data['id']; ?>" class="productcontainerviewmore">Read More</a>
-                    <a href="delete.php?id=<?php echo $data['id']; ?>" class="productcontainerdelete">Delete</a>
+
+             <tr>
+
+                <td><?php echo $data ['id'];?></td>
+                <td><img src="<?php echo $data ['Image'];?>"></td><!--image-->
+                <td><?php echo $data ['Name'];?></td><!--name-->
+                <td><?php echo $data ['Price'];?></td><!--price-->
+                <td>
+                    <a href="view.php?id=<?php echo $data['id']; ?>" > More</a>
+                    <a href="delete.php?id=<?php echo $data['id']; ?>" >Delete</a>
+
                 </td>
+
              </tr>
 
             <?php
-        ?>        
+        }?>        
+
+
         </tbody>
 
 
     </table>
 
-        <footer>
-            <div class="footercontainer">
-                <p>All copy rights recived </p>
-            </div>
-        </footer>
+
 
 </body>
 </html>
